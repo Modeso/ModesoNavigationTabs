2f94f7916c71ca8632b845509346729e5326df51
Mohammed Elsammak <mohammed.elsammak@modeso.ch>
2017-03-23 17:30:31 +0200

    Adding ViewControllers scrollviews

--- MNavigationTabs/Sources/MNavigationTabsViewController.swift
+++ MNavigationTabs/Sources/MNavigationTabsViewController.swift
@@ -10,24 +10,48 @@ import UIKit
 
 public class MNavigationTabsViewController: UIViewController {
 
-    var color: UIColor!
+    var color: UIColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+    var tabWidth: CGFloat = 111.0
+    var viewControllersArray: [UIViewController] = [] {
+        didSet {
+            adjustviewControllersScrollView()
+        }
+    }
+    var viewControllersTitlesArray: [String] = []
+    
+    
+    @IBOutlet weak var tabsScrollView: UIScrollView!
+    @IBOutlet weak var viewControllersScrollView: UIScrollView!
+    
     @IBOutlet weak var label: UILabel!
     override public func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-        label.textColor = color
     }
 
     override public func loadView() {
         super.loadView()
         Bundle(for: MNavigationTabsViewController.self).loadNibNamed("MNavigationTabsViewController", owner: self, options: nil)
     }
+    
+    private func configureTabsMenuView() {
+        
+    }
+    
+    func adjustviewControllersScrollView() {
+        var origin: CGFloat = 0.0
+        for viewController in viewControllersArray {
+            viewController.view.frame = CGRect(x: origin, y: 0.0, width: viewController.view.frame.size.width, height: viewController.view.frame.size.height)
+            viewControllersScrollView.addSubview(viewController.view)
+            origin += viewController.view.frame.size.width
+        }
+        viewControllersScrollView.contentSize = CGSize(width: origin, height: self.view.frame.size.height)
+    }
 
 }
 
 @IBDesignable extension MNavigationTabsViewController {
-    @IBInspectable var labelTextColor: UIColor? {
+    @IBInspectable var labelTextColor: UIColor {
         set {
             color = newValue
         }
